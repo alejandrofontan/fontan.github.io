24efb1e7e9e3710c1550b23436ac6e848f74da8e
alejandrofontan <fontanvillacampa@gmail.com>
2025-08-31 15:02:09 +1000

    rss25

--- publications/news.js
+++ publications/news.js
@@ -16,11 +16,11 @@ const pubNews = [
   //   "caption": "I presented my research on Spatio-Temporal AI at the Georgia Institute of Technology!"
   // },
   {
-    "title": "We organized a <a href='https://vslam-lab.github.io/Unifying-VSLAM-RSS25/'/>Workshop</a> at <a href='https://roboticsconference.org/'/>RSS25</a> in Los Angeles D. The recordings of all workshop talks are now available on <a href='https://www.youtube.com/playlist?list=PLm2jyT49-dYAZKv2aK-XLch3zqg8bOAks'/>Youtube</a>!",
+    "title": "We organized a <a href='https://vslam-lab.github.io/Unifying-VSLAM-RSS25/'/>Workshop</a> at <a href='https://roboticsconference.org/'/>RSS25</a> in Los Angeles. The recordings of all workshop talks are now available on <a href='https://www.youtube.com/playlist?list=PLm2jyT49-dYAZKv2aK-XLch3zqg8bOAks'/>Youtube</a>!",
     "date": "3/2025",
     "image": "rss25_ws.jpg",
     "thumb": "rss25_ws.jpg",
-    "caption": "We organized a <a href='https://vslam-lab.github.io/Unifying-VSLAM-RSS25/'/>Workshop</a> at <a href='https://roboticsconference.org/'/>RSS25</a> in Los Angeles D. The recordings of all workshop talks are now available on <a href='https://www.youtube.com/playlist?list=PLm2jyT49-dYAZKv2aK-XLch3zqg8bOAks'/>Youtube</a>!"
+    "caption": "We organized a <a href='https://vslam-lab.github.io/Unifying-VSLAM-RSS25/'/>Workshop</a> at <a href='https://roboticsconference.org/'/>RSS25</a> in Los Angeles. The recordings of all workshop talks are now available on <a href='https://www.youtube.com/playlist?list=PLm2jyT49-dYAZKv2aK-XLch3zqg8bOAks'/>Youtube</a>!"
   }
   // {
   //   "title": "I presented my research on Spatio-Temporal AI at Boston University!",
